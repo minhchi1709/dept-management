b6198e737ba9d05d2994435f57f4e3761f24e32d
minhchis <diepminhchi1617@gmail.com>
2024-09-27 23:44:41 +0700

    OpenAPI configuration

--- DebtManagement/src/main/java/vn/diepgia/mchis/DebtManagement/DebtManagementApplication.java
+++ DebtManagement/src/main/java/vn/diepgia/mchis/DebtManagement/DebtManagementApplication.java
@@ -1,8 +1,36 @@
 package vn.diepgia.mchis.DebtManagement;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(
+		info = @Info(
+				contact = @Contact(
+						name = "Minh Chi Diep",
+						email = "diepminhchi1617@gmail.com",
+						url = "https://minhchi1709.github.io/mywebsite/index.html"
+				),
+				description = "OpenApi documentation for debt management",
+				title = "OpenApi specification - Minh Chi Diep",
+				version = "1.0",
+				license = @License(
+						name = "Licence name",
+						url = "https://some-url.com"
+				),
+				termsOfService = "Terms of service"
+		),
+		servers = {
+				@Server(
+						description = "Local ENV",
+						url = "http://localhost:8088/app/DebtManagement/api"
+				)
+		}
+)
 @SpringBootApplication
 public class DebtManagementApplication {
 
