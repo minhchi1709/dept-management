2319c2392a966e0723e5f52a14f6f5ccf9f795ce
unknown <diepminhchi1617@gmail.com>
2024-09-27 14:48:01 +0700

    initialize backend

---
+++ DebtManagement/src/main/java/vn/diepgia/mchis/DebtManagement/DebtManagementApplication.java
@@ -0,0 +1,13 @@
+package vn.diepgia.mchis.DebtManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DebtManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DebtManagementApplication.class, args);
+	}
+
+}
---
+++ DebtManagement/src/test/java/vn/diepgia/mchis/DebtManagement/DebtManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.diepgia.mchis.DebtManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DebtManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
